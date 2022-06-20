bd1f4b2f61c477a5390726a05cc141fb19a1e9eb
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-06-20 18:45:41 +0300

    dz24
    dz24

--- Program.cs
+++ Program.cs
@@ -8,37 +8,70 @@ namespace vn_mode_csharp_dz24
         {
             int[] arrayNumbers = new int[30];
             int minNumberForRandom = 0;
-            int maxNumberForRandom = 6;
-            int tempNumber = 0, counterMaxNumber = 0;
+            int maxNumberForRandom = 30;
+            int maxRepeatNumber = minNumberForRandom -1;
+            int counterMaxRepeatNumber = 1;
+            int tempCounterNumber = 1;
             Random randomNumber = new Random();
 
             for (int i = 0; i < arrayNumbers.Length; i++)
             {
                 arrayNumbers[i] = randomNumber.Next(minNumberForRandom, maxNumberForRandom);
             }
-            tempNumber = arrayNumbers[0];
+
             Console.Write("Исходный массив чисел: ");
 
-            foreach (int number in arrayNumbers)
+            foreach (int item in arrayNumbers)
             {
-                Console.Write(number + " ");
+                Console.Write(item + " ");
             }
-            foreach (int number in arrayNumbers)
+
+            for (int i = 0; i < arrayNumbers.Length; i++)
             {
-                if(tempNumber < number)
+                for (int j = 0; j < arrayNumbers.Length; j++)
                 {
-                    tempNumber = number;
+                    int tempNumber;
+                    if (arrayNumbers[i] < arrayNumbers[j])
+                    {
+                        tempNumber = arrayNumbers[i];
+                        arrayNumbers[i] = arrayNumbers[j];
+                        arrayNumbers[j] = tempNumber;
+                    }
                 }
             }
-            foreach (int number in arrayNumbers)
+
+            for (int i = 0; i < arrayNumbers.Length; i++)
             {
-                if (tempNumber == number)
+                if ((i+1) < arrayNumbers.Length)
                 {
-                    counterMaxNumber++;
+                    if (arrayNumbers[i] == arrayNumbers[i + 1])
+                    {
+                        tempCounterNumber++;
+
+                        if (tempCounterNumber > counterMaxRepeatNumber)
+                        {
+
+                            counterMaxRepeatNumber = tempCounterNumber;
+                            maxRepeatNumber = arrayNumbers[i];
+                        }
+                    }
+
+                    if (arrayNumbers[i] < arrayNumbers[i + 1])
+                    {
+                        tempCounterNumber = 1;
+                    }
                 }
+            }
+
+            if (maxRepeatNumber == (minNumberForRandom - 1))
+            {
+                Console.WriteLine("\nВ массиве нет повторяющихся чисел.");
+            }
+            else
+            {
+                Console.WriteLine($"\nЧисло {maxRepeatNumber} чаще других встречается в массиве и повторяется {counterMaxRepeatNumber} раз/раза.");
             }
             
-            Console.WriteLine($"\nЧисло {tempNumber} самое большое в массиве и повторяется {counterMaxNumber} раз/раза.");
         }
     }
 }
